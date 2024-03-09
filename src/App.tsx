f9e0057a0bfbc51965d32558b6ac674c93d0b284
huangtonghao <a123>
2024-03-09 11:53:39 +0800

    feat: add zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
+import { Button, Space } from 'antd'
 
 import { listDemo } from '@/api/demo'
 import { AEmpty } from '@/components'
+import { useGlobalStore } from '@/store'
+
+import './styles/App.less'
 
 function App() {
+  const { count, increment, decrement } = useGlobalStore()
   const [list, setList] = useState<any>([])
 
   const fetchData = async () => {
@@ -16,16 +21,27 @@ function App() {
   }, [])
 
   return (
-    <>
+    <div className="app-body">
       <AEmpty />
+
+      <h3>{count}</h3>
+      <Space>
+        <Button onClick={() => increment(1)} type="primary">
+          count +
+        </Button>
+        <Button onClick={() => decrement(1)} type="primary">
+          count -
+        </Button>
+      </Space>
+
       {list && (
         <div>
-          {list.map((item: any) => (
+          {list.slice(0, 3).map((item: any) => (
             <h3 key={item.id}>{item.title}</h3>
           ))}
         </div>
       )}
-    </>
+    </div>
   )
 }
 
---
+++ src/store/useGlobalStore.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand'
+
+type State = {
+  count: number
+}
+
+type Actions = {
+  increment: (qty: number) => void
+  decrement: (qty: number) => void
+}
+
+export const useGlobalStore = create<State & Actions>((set) => ({
+  count: 1,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 }))
+}))
