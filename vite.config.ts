1410ff3d7f04c23ba74bf3c90337d800a41a4ee0
norush <a123>
2024-03-26 10:44:09 +0800

    fix: 添加 .env 环境变量配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,38 @@
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [
-      { find: '@', replacement: resolve(__dirname, 'src') },
-      { find: /^~/, replacement: '' }
-    ]
-  },
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    resolve: {
+      alias: [
+        { find: '@', replacement: resolve(__dirname, 'src') },
+        { find: /^~/, replacement: '' }
+      ],
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs', '.cjs', '.json']
+    },
+
+    /**
+     * 环境变量代理
+     */
+    server: {
+      proxy: {
+        '/dev': {
+          target: env.VITE_APP_BASE_URL,
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/device-api/, '')
+        },
+        '/pro': {
+          target: env.VITE_APP_BASE_URL,
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/product-api/, '')
+        }
+      }
+    },
+
+    plugins: [react()]
+  }
 })
